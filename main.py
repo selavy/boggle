45e6dd93f0c5519a61971ba091ef7aab376499df
Peter Lesslie <pclesslie@gmail.com>
2015-11-28 22:24:06 -0600

    GUI version

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import sys
 import time
 import sched
+from Tkinter import *
 
 cubes = [
     'AAEEGN',
@@ -26,19 +27,28 @@ cubes = [
 
 if __name__ == '__main__':
     random.shuffle(cubes)
+    root = Tk()
+
+
+    row = 0
     for i, cube in enumerate(cubes):
-        if i % 4 == 0:
+        if i % 4 == 0 and i != 0:
             sys.stdout.write('\n')
-        sys.stdout.write(random.choice(cube) + ' ')
+            row += 1
+        letter = random.choice(cube)
+        sys.stdout.write(letter + ' ')
+        Label(root, text=letter, font=("Helvetica", 16)).grid(row=row, column=(i % 4), ipady=20, ipadx=20)
+    
     sys.stdout.write('\n\n')
 
-    time.sleep(3)
-    print 'Start!'
-
-    def stop():
-        print "Stop!"
-    s = sched.scheduler(time.time, time.sleep)
-    s.enter(3*60, 1, stop, ())
-    s.run()
+    root.mainloop()
+#    time.sleep(3)
+#    print 'Start!'
+#
+#    def stop():
+#        print "Stop!"
+#    s = sched.scheduler(time.time, time.sleep)
+#    s.enter(3*60, 1, stop, ())
+#    s.run()
     
 
